f99e42856cf2e654005437ce361f1b0b0deaf2f6
HassanAlee <hassanali14563@gmail.com>
2025-07-01 11:09:36 +0500

    setup the theme for application

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,10 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function page() {
-  return <Button>Shadcn Button</Button>;
+  return (
+    <>
+      <h1>Sommaire</h1>
+      <Button variant={"outline"}>Shadcn Button</Button>;
+    </>
+  );
 }
