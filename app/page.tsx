11c32534fefac9c88f14cebced9d906df686d130
HassanAlee <hassanali14563@gmail.com>
2025-08-20 18:25:09 +0500

    completed summary section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+import BgGradient from "@/components/common/bg-gradient";
+import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
-import { Button } from "@/components/ui/button";
 
 import React from "react";
 
 export default function page() {
   return (
     <div className="relative w-full">
-      <HeroSection />
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+      </div>
     </div>
   );
 }
